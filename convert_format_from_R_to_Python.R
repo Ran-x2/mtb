6c1c82eadcf74c9869afb70a7d7ad278dce9c806
Ran-x2 <rxr456@case.edu>
2025-04-02 23:46:46 -0400

    Up

--- convert_format_from_R_to_Python.R
+++ convert_format_from_R_to_Python.R
@@ -7,13 +7,43 @@ library(dplyr)
 library(SeuratDisk)
 library(SeuratData)
 library(rhdf5)
-
 setwd('E:/AAA_Labwork/capenterlab_mtb')
+
 Convert("SeuratProject.h5Seurat", dest = "h5ad")
 
 H5list = h5ls("SeuratProject.h5Seurat") #there is an error by just reading the
 gene_names = h5read("SeuratProject.h5Seurat",'/assays/SCT/features')
 write.table(data.frame(gene_names), 'var_names.csv', sep = ',')
 
+metadata = h5read("SeuratProject.h5Seurat",'/meta.data')
+cell_names = metadata$`_index`
+
 SCT_corrected_counts = h5read("SeuratProject.h5Seurat",'/assays/SCT/counts')
 saveRDS(SCT_corrected_counts,'SCT_corrected_counts')
+SCT_corrected_counts = readRDS('SCT_corrected_counts')
+
+n_rows <- 20395
+n_cols <- 157462
+nonzero_values <- as.numeric(SCT_corrected_counts$data)
+row_indices <- SCT_corrected_counts$indices + 1  
+sparse_mat <- sparseMatrix(
+  i = row_indices,         
+  p = SCT_corrected_counts$indptr,  
+  x = nonzero_values,    
+  dims = c(n_rows, n_cols)
+)
+
+colnames(sparse_mat) = as.character(cell_names)
+row.names(sparse_mat) = as.character(gene_names)
+srat_for_DEG = CreateSeuratObject(counts = sparse_mat, project = "mtb", min.cells = 0, min.features = 0, assay = "RNA")
+saveRDS(srat_for_DEG,'srat_for_DEG.rds')
+
+srat_for_DEG@meta.data$seurat_clusters = metadata$seurat_clusters
+clusters_to_keep <- c("0", "3", "6", "12", "14")
+srat_sub <- subset(srat_for_DEG, subset = seurat_clusters %in% clusters_to_keep)
+srat_sub$expansion <- ifelse(srat_sub$seurat_clusters %in% c("0", "3"), "less", "more")
+
+# Check the new metadata column
+head(srat_sub@meta.data)
+saveRDS(srat_for_DEG,'srat_subset_for_DEG.rds')
+
