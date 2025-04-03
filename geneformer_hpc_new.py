f57bfcde850900d00de715f8a880a4fcfc6050f0
Ran Ran <112669569+Ran-x2@users.noreply.github.com>
2025-04-02 22:03:58 -0400

    update

---
+++ geneformer_hpc_new.py
@@ -0,0 +1,76 @@
+import sys
+import os
+sys.path.append(os.getcwd())
+from geneformer import InSilicoPerturber
+from geneformer import InSilicoPerturberStats
+from geneformer import EmbExtractor
+import pickle
+
+storage_dir = '/mnt/vstor/SOM_PATH_DKB50/members/rxr456/mtb_new/'
+output_prefix="cell_state_shift_6000"
+vanilla_model = "/home/rxr456/Geneformer/gf-12L-95M-i4096"
+
+from geneformer import TranscriptomeTokenizer
+tk = TranscriptomeTokenizer({"expansion": "expansion"}, nproc=15)
+tk.tokenize_data(f"{storage_dir}", 
+                 f"{storage_dir}",
+                 "tokenized", 
+                 file_format="h5ad")
+
+from geneformer import Classifier
+cc = Classifier(classifier="cell",
+                cell_state_dict = {"state_key": "expansion", "states": "all"},
+                max_ncells=None,
+                freeze_layers = 6,
+                num_crossval_splits = 1,
+                split_sizes = {"train": 0.6, "valid": 0.2, "test": 0.2},
+                forward_batch_size=150,
+                nproc=47)
+
+
+cc.prepare_data(input_data_file=f"{storage_dir}/tokenized.dataset",
+                output_directory=f"{storage_dir}/",
+                output_prefix=output_prefix)
+
+all_metrics = cc.validate(model_directory="/home/rxr456/Geneformer/gf-12L-95M-i4096",
+                          prepared_input_data_file=f"{storage_dir}/{output_prefix}_labeled_train.dataset",
+                          id_class_dict_file=f"{storage_dir}/{output_prefix}_id_class_dict.pkl",
+                          output_directory=f"{storage_dir}/",
+                          output_prefix=output_prefix,
+                          #n_hyperopt_trials=1,
+                          predict_eval=True)
+
+embex = EmbExtractor(model_type="CellClassifier",
+                     num_classes=2, 
+                     max_ncells=10000,
+                     emb_layer=-1, 
+                     emb_label=["expansion"],
+                     labels_to_plot=["expansion"],
+                     forward_batch_size=150,
+                     nproc=80)
+
+
+embs = embex.extract_embs(model,
+                          f"{storage_dir}/tokenized.dataset",
+                          f"{storage_dir}/",
+                          output_prefix + "_embeddings_labeled")
+
+embex.plot_embs(embs=embs,
+                plot_style="heatmap",
+                output_directory=f"{storage_dir}/",
+                output_prefix="embeddings_heatmap")
+
+
+# all_metrics_test = cc.evaluate_saved_model(
+#         model_directory=model,
+#         id_class_dict_file=f"{storage_dir}/{output_prefix}_id_class_dict.pkl",
+#         test_data_file=f"{storage_dir}/{output_prefix}_labeled_test.dataset",
+#         output_directory=f"{storage_dir}/",
+#         output_prefix=output_prefix + output_prefix,
+#     )
+
+cc.plot_conf_mat(
+        conf_mat_dict={"Geneformer": all_metrics["conf_matrix"]},
+        output_directory=f"{storage_dir}/",
+        output_prefix=output_prefix + output_prefix
+)
